0b002bca990a466b5f085f63c229a0260e02e0ee
Dmitriy Efimov <efimov@qtech.ru>
2025-09-20 16:06:42 +0300

    Remove base64. Resize with RGA driver

--- examples/zmq/cpp/main.cc
+++ examples/zmq/cpp/main.cc
@@ -10,20 +10,26 @@
 #include "file_utils.h"
 #include "image_drawing.h"
 
-
 using namespace cv;
 using namespace std;
+
+#define SRC_WIDTH 640
+#define SRC_HEIGHT 480
+#define FPS 30
+
+#define WIDTH 640
+#define HEIGHT 640
 
 static const unsigned char base64_table[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 /**
-* base64_encode - Base64 encode
-* @src: Data to be encoded
-* @len: Length of the data to be encoded
-* @out_len: Pointer to output length variable, or %NULL if not used
-* Returns: Allocated buffer of out_len bytes of encoded data,
-* or empty string on failure
-*/
+ * base64_encode - Base64 encode
+ * @src: Data to be encoded
+ * @len: Length of the data to be encoded
+ * @out_len: Pointer to output length variable, or %NULL if not used
+ * Returns: Allocated buffer of out_len bytes of encoded data,
+ * or empty string on failure
+ */
 std::string base64_encode(const unsigned char *src, size_t len)
 {
     unsigned char *out, *pos;
@@ -31,19 +37,20 @@ std::string base64_encode(const unsigned char *src, size_t len)
 
     size_t olen;
 
-    olen = 4*((len + 2) / 3); /* 3-byte blocks to 4-byte */
+    olen = 4 * ((len + 2) / 3); /* 3-byte blocks to 4-byte */
 
     if (olen < len)
         return std::string(); /* integer overflow */
 
     std::string outStr;
     outStr.resize(olen);
-    out = (unsigned char*)&outStr[0];
+    out = (unsigned char *)&outStr[0];
 
     end = src + len;
     in = src;
     pos = out;
-    while (end - in >= 3) {
+    while (end - in >= 3)
+    {
         *pos++ = base64_table[in[0] >> 2];
         *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
         *pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
@@ -51,21 +58,49 @@ std::string base64_encode(const unsigned char *src, size_t len)
         in += 3;
     }
 
-    if (end - in) {
+    if (end - in)
+    {
         *pos++ = base64_table[in[0] >> 2];
-        if (end - in == 1) {
+        if (end - in == 1)
+        {
             *pos++ = base64_table[(in[0] & 0x03) << 4];
             *pos++ = '=';
         }
-        else {
+        else
+        {
             *pos++ = base64_table[((in[0] & 0x03) << 4) |
-                (in[1] >> 4)];
+                                  (in[1] >> 4)];
             *pos++ = base64_table[(in[1] & 0x0f) << 2];
         }
         *pos++ = '=';
     }
 
     return outStr;
+}
+
+bool mat_to_image_buffer(const cv::Mat &mat, image_buffer_t &out_buf, image_format_t fmt)
+{
+    if (!mat.data || mat.empty())
+    {
+        return false;
+    }
+
+    if (!mat.isContinuous())
+    {
+        // Неоднородные данные не могут передаваться напрямую
+        return false;
+    }
+
+    out_buf.width = mat.cols;
+    out_buf.height = mat.rows;
+    out_buf.width_stride = static_cast<int>(mat.step); // Байтов на строку
+    out_buf.height_stride = mat.rows;
+    out_buf.format = fmt;
+    out_buf.virt_addr = mat.data;
+    out_buf.size = mat.total() * mat.elemSize();
+    out_buf.fd = -1; // Если есть fd, его можно назначить отдельно
+
+    return true;
 }
 
 int main(int argc, char **argv)
@@ -86,9 +121,9 @@ int main(int argc, char **argv)
         int cam = static_cast<int>(strtol(source, nullptr, 10));
         cout << "Before open camera: " << cam << endl;
         vid.open(cam);
-        vid.set(CAP_PROP_FRAME_WIDTH, 640);
-        vid.set(CAP_PROP_FRAME_HEIGHT, 480);
-        vid.set(CAP_PROP_FPS, 30);
+        vid.set(CAP_PROP_FRAME_WIDTH, SRC_WIDTH);
+        vid.set(CAP_PROP_FRAME_HEIGHT, SRC_HEIGHT);
+        vid.set(CAP_PROP_FPS, FPS);
         cout << "After open camera" << endl;
     }
     else
@@ -112,10 +147,10 @@ int main(int argc, char **argv)
         return 2;
     }
 
-    resize(m, mat, Size(640, 640), 0, 0, INTER_AREA);
-    size_t mat_len = mat.total() * mat.elemSize();
-    cout << "Type: " << mat.type() << endl;
-    cout << "Size: " << mat_len << endl;
+    // resize(m, mat, Size(WIDTH, HEIGHT), 0, 0, INTER_AREA);
+    // size_t mat_len = mat.total() * mat.elemSize();
+    // cout << "Type: " << mat.type() << endl;
+    // cout << "Size: " << mat_len << endl;
 
     int ret;
     rknn_app_context_t rknn_app_ctx;
@@ -129,15 +164,16 @@ int main(int argc, char **argv)
         cerr << "init_yolov5_model fail! ret = " << ret << " model_path = " << model_path << endl;
         vid.release();
 
-		deinit_post_process();
+        deinit_post_process();
 
-		ret = release_yolov5_model(&rknn_app_ctx);
-		if (ret != 0) { cout << "release_yolov5_model fail! ret = " << ret << endl; }
+        ret = release_yolov5_model(&rknn_app_ctx);
+        if (ret != 0)
+        {
+            cout << "release_yolov5_model fail! ret = " << ret << endl;
+        }
     }
 
-    image_buffer_t src_image;
-    memset(&src_image, 0, sizeof(image_buffer_t));
-
+    image_buffer_t src_image, dst_image;
 
     zmq::context_t ctx;
     zmq::socket_t sock(ctx, zmq::socket_type::pub);
@@ -148,22 +184,40 @@ int main(int argc, char **argv)
     static int f = 0;
     static int cnt = 0;
 
-    while(true)
+    while (true)
     {
         start = clock();
 
-        vid >> m; if (m.empty()) { cerr << "ERROR: Image is empty" << endl; break; }
+        vid >> m;
+        if (m.empty())
+        {
+            cerr << "ERROR: Image is empty" << endl;
+            break;
+        }
 
-	string msg;
+        string msg;
 
-        resize(m, mat, Size(640, 640), 0, 0, INTER_AREA);
-        mat_len = mat.total() * mat.elemSize();
+        // resize(m, mat, Size(640, 640), 0, 0, INTER_AREA);
+        // mat_len = mat.total() * mat.elemSize();
 
-        src_image.width = 640;
-        src_image.height = 640;
-        src_image.size = mat_len;
-        src_image.virt_addr = mat.data;
-        src_image.format = IMAGE_FORMAT_RGB888;
+        // src_image.width = 640;
+        // src_image.height = 640;
+        // src_image.size = mat_len;
+        // src_image.virt_addr = mat.data;
+        // src_image.format = IMAGE_FORMAT_RGB888;
+
+        memset(&src_image, 0, sizeof(image_buffer_t));
+        if (!mat_to_image_buffer(m, src_image, IMAGE_FORMAT_RGB888))
+        {
+            cerr << "Mat to image buffer failed" << endl;
+            continue;
+        }
+
+        memset(&dst_image, 0, sizeof(image_buffer_t));
+        dst_image.width = WIDTH;
+        dst_image.height = HEIGHT;
+
+        convert_image(src_image, dst_image, NULL, NULL, 0);
 
         object_detect_result_list od_results;
 
@@ -171,49 +225,46 @@ int main(int argc, char **argv)
         if (ret != 0)
         {
             cerr << "init_yolov5_model fail! ret = " << ret << endl;
-	    	break;
+            break;
         }
 
         for (int i = 0; i < od_results.count; i++)
         {
-	    char text[256];
+            char text[256];
             object_detect_result *det_result = &(od_results.results[i]);
-//            printf("%s @ (%d %d %d %d) %.3f\n", coco_cls_to_name(det_result->cls_id),
-//                   det_result->box.left, det_result->box.top,
-//                   det_result->box.right, det_result->box.bottom,
-//                   det_result->prop);
+            //            printf("%s @ (%d %d %d %d) %.3f\n", coco_cls_to_name(det_result->cls_id),
+            //                   det_result->box.left, det_result->box.top,
+            //                   det_result->box.right, det_result->box.bottom,
+            //                   det_result->prop);
             int x1 = det_result->box.left;
             int y1 = det_result->box.top;
             int x2 = det_result->box.right;
             int y2 = det_result->box.bottom;
 
-//          draw_rectangle(&src_image, x1, y1, x2 - x1, y2 - y1, COLOR_BLUE, 3);
-
-//          sprintf(text, "%s %.1f%%", coco_cls_to_name(det_result->cls_id), det_result->prop * 100);
-//          draw_text(&src_image, text, x1, y1 - 20, COLOR_RED, 10);
+            // draw_rectangle(&src_image, x1, y1, x2 - x1, y2 - y1, COLOR_BLUE, 3);
+            // sprintf(text, "%s %.1f%%", coco_cls_to_name(det_result->cls_id), det_result->prop * 100);
+            // draw_text(&src_image, text, x1, y1 - 20, COLOR_RED, 10);
 
             sprintf(text, "%s@%d,%d,%d,%d@%.2f;", coco_cls_to_name(det_result->cls_id),
-	     	   		det_result->box.left, det_result->box.top, det_result->box.right, det_result->box.bottom, det_result->prop);
+                    det_result->box.left, det_result->box.top, det_result->box.right, det_result->box.bottom, det_result->prop);
 
             msg += text;
-//            sock.send(zmq::buffer(text), zmq::send_flags::dontwait);
+            sock.send(zmq::buffer(text), zmq::send_flags::sndmore);
         }
-	
-	vector<uchar> buffer;
-	imencode(".jpg", mat, buffer);
 
-        msg += base64_encode(buffer.data(), buffer.size());
-        sock.send(zmq::buffer(msg), zmq::send_flags::dontwait);
+        zmq::message_t img(mat.data, mat_len);
+        sock.send(img, zmq::send_flags::dontwait);
 
         end = clock();
-        double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
         t += cpu_time_used;
         f += 1;
 
         if (f % 30 == 0)
         {
             cout << "\t FPS: " << std::fixed << std::setw(11) << std::setprecision(6) << 30.0 / t << " time = " << t << endl;
-            t = 0; f = 0;
+            t = 0;
+            f = 0;
         }
     }
 
@@ -223,9 +274,15 @@ int main(int argc, char **argv)
     deinit_post_process();
 
     ret = release_yolov5_model(&rknn_app_ctx);
-    if (ret != 0) { cout << "release_yolov5_model fail! ret = " << ret << endl; }
+    if (ret != 0)
+    {
+        cout << "release_yolov5_model fail! ret = " << ret << endl;
+    }
 
-    if (src_image.virt_addr != NULL) { free(src_image.virt_addr); }
+    if (src_image.virt_addr != NULL)
+    {
+        free(src_image.virt_addr);
+    }
 
     return 0;
 }
